d1e49761aac4f79266f95fd4a6897d3825b9635f
Valentín Galfré <galfre.vn@gmail.com>
2023-08-12 00:38:52 -0300

    Removing unused environemnt variables configuration

--- environment.mjs
+++ environment.mjs
@@ -2,15 +2,10 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 export const environmentVariables = createEnv({
-  runtimeEnv: {
-    ...process.env,
-    NEXT_PUBLIC_GIPHY_KEY: process.env.NEXT_PUBLIC_GIPHY_KEY,
-  },
-  client: {
-    NEXT_PUBLIC_GIPHY_KEY: z.string().min(1),
-  },
+  runtimeEnv: process.env,
   server: {
     DATABASE_URL: z.string().url(),
+
     NEXTAUTH_SECRET: z.string().min(1),
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
